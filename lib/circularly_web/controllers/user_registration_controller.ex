c0f23347440c73f3a603901141a3983fa2d34c34
Carsten Block <mail@carstenblock.org>
2021-12-29 21:18:37 +0100

    Add i18n to sign-up/ sign-in screens

--- lib/circularly_web/controllers/user_registration_controller.ex
+++ lib/circularly_web/controllers/user_registration_controller.ex
@@ -20,7 +20,7 @@ defmodule CircularlyWeb.UserRegistrationController do
           )
 
         conn
-        |> put_flash(:info, "User created successfully.")
+        |> put_flash(:info, dgettext("auth", "User created successfully."))
         |> UserAuth.log_in_user(user)
 
       {:error, %Ecto.Changeset{} = changeset} ->
